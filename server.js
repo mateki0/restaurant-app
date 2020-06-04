ae3e3f4632a5e33e88e192e3b099e592c6a249e0
mateki0 <mateki0@interia.pl>
2020-06-04 14:35:28 +0200

    path changed

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html', 'index.html'));
 });
 app.use(morgan('dev')) // log req to console
 app.use(cookieParser());
