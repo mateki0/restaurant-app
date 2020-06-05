9b52dd21125fd679433e562601b9dc62fb0ae237
mateki0 <mateki0@interia.pl>
2020-06-05 09:11:59 +0200

    git ignore change

---
+++ client/build/precache-manifest.dfe37c2e7466520a81ccdbef29faec14.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bcdf12f5bf0516fa5318de13650a59a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd46974434eaa8c086fb",
+    "url": "/static/css/2.b0d70484.chunk.css"
+  },
+  {
+    "revision": "14c8935a9f2bc2e0991e",
+    "url": "/static/css/main.99a021b9.chunk.css"
+  },
+  {
+    "revision": "dd46974434eaa8c086fb",
+    "url": "/static/js/2.c3f45e02.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c3f45e02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14c8935a9f2bc2e0991e",
+    "url": "/static/js/main.627ba17f.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "46aa2010a39a9297f9923235ac6d62f7",
+    "url": "/static/media/burger.46aa2010.jpg"
+  },
+  {
+    "revision": "d91242c5876914f78d5e58f2472c1c32",
+    "url": "/static/media/chicken.d91242c5.jpg"
+  },
+  {
+    "revision": "2009ca9158e4631857ea1ae00b78e3ea",
+    "url": "/static/media/drink.2009ca91.jpg"
+  },
+  {
+    "revision": "1cfc3642e0d89eac5ff9b7d2b71fc47a",
+    "url": "/static/media/eggs.1cfc3642.jpg"
+  },
+  {
+    "revision": "d8f303ae76aebdb61f4f2f387cf80518",
+    "url": "/static/media/pancakes.d8f303ae.jpg"
+  },
+  {
+    "revision": "e3c5ad60b4dc1f9b7780f2e95cb53d2b",
+    "url": "/static/media/pasta.e3c5ad60.jpg"
+  },
+  {
+    "revision": "ec46b7c3212c128a002a828a57e0b7be",
+    "url": "/static/media/salad.ec46b7c3.jpg"
+  }
+]);
